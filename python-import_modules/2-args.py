eebb70cb436068dc6243fd0a0b0c788aae12d2be
Bree-Coding <brendapollagba@gmail.com>
2025-01-16 22:56:30 +0100

    fix: update calculation functions in main script and improve argument output formatting

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
+
     number = len(sys.argv) - 1
+
     if number == 0:
         print("0 arguments.")
     elif number == 1:
         print("1 arguments.")
     else:
         print("{} arguments".format(number, end=""))
+
     for x in range(number):
         print("{}: {}".format(x + 1, sys.argv[x+1]))
