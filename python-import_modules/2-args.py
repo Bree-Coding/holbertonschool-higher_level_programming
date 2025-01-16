09292071f6a2ee045fbe2419f087cae469df259d
Bree-Coding <brendapollagba@gmail.com>
2025-01-16 21:43:53 +0100

    added tasks for review

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv) -1
+    if number == 0:
+        print("0 arguments.")
+    elif number == 1:
+        print("1 arguments.")
+    else:
+        print("{} arguments".format(number))
+    for x in range (number):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
+    
