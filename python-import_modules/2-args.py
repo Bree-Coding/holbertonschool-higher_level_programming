286213352787b0716e97538832cabf47af0edd45
Bree-Coding <brendapollagba@gmail.com>
2025-01-16 22:50:10 +0100

    fix: correct spacing and formatting in calculation and argument scripts

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    number = len(sys.argv) -1
+    number = len(sys.argv) - 1
     if number == 0:
         print("0 arguments.")
     elif number == 1:
         print("1 arguments.")
     else:
-        print("{} arguments".format(number , end=""))
+        print("{} arguments".format(number, end=""))
     for x in range(number):
         print("{}: {}".format(x + 1, sys.argv[x+1]))
