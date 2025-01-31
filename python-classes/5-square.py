79b4bbe0366e01d887b53247b5cffe53c138cbfe
Bree-Coding <brendapollagba@gmail.com>
2025-01-31 16:37:27 +0100

    refactor: update docstrings for Square class methods and attributes

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-"""class that define a square with a private instance attribute: size"""
+"""class that define a square with a public instance method: def area(self),
+a private instance attribute: size and Private instance attribute: position"""
 
 
 class Square:
 
     """__init__ initializes the size of the square
         size = 0, which referses to the size of the square
+        value is the new size of the square.
         size must be an integer, otherwise TypeError
-        size must be >= 0, otherwise ValueError
-        return the square area"""
+        size must be >= 0, otherwise ValueError"""
 
     def __init__(self, size=0):
         if type(size) is not int:
@@ -30,9 +31,11 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    """Return the current squarearea"""
     def area(self):
         return self.__size ** 2
 
+    """Prints the square with the character '#'. If the size is 0, it prints an empty line."""
     def my_print(self):
         if self.__size > 0:
             for x in range(self.__size):
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-"""class that define a square with a private instance attribute: size"""
+"""class that define a square with a public instance method: def area(self),
+a private instance attribute: size and Private instance attribute: position"""
 
 
 class Square:
 
     """__init__ initializes the size of the square
         size = 0, which referses to the size of the square
-        position is horizontal and vertical lines
+        position is a tuple and the position of the square
+        value is the new size of the square
         size must be an integer, otherwise TypeError
-        size must be >= 0, otherwise ValueError
-        return the square area"""
+        size must be >= 0, otherwise ValueError"""
 
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
@@ -34,16 +35,18 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) is not 2:
+        if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         for num in value:
             if type(num) is not int or num < 0:
                 raise TypeError("position must be a tuple of 2 positive integers")
-        self.__size = value
+        self.__position = position
 
+    """Return the current square area"""
     def area(self):
         return self.__size ** 2
 
+    """Prints the square with the character '#'. If the size is 0, it prints an empty line"""
     def my_print(self):
         if self.__size > 0:
             for x in range(self.__position[1]):
