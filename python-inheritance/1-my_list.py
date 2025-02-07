fc2ffbda28c35c1498ddfd3a97c6f29899422a90
Bree-Coding <brendapollagba@gmail.com>
2025-02-07 20:36:36 +0100

    fixed task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-"""Module that defines a subclass of list with a method to print the list in sorted order."""
+"""Module that defines a subclass of list with a method
+to print the list in sorted order."""
 
 
 class MyList(list):
     """A subclass of list that provides a method
     to print the list in sorted order.
     """
+    pass
+
     def print_sorted(self):
         """Prints the list in ascending sorted order."""
         print(sorted(self))
