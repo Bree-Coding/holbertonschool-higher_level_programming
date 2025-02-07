3c38c5d301efd8780b9977dbd817d1b13a4faedb
Bree-Coding <brendapollagba@gmail.com>
2025-02-07 21:58:31 +0100

    update file permissions and improve test cases for clarity

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,6 @@
 is an instance of a class that inherited (directly or indirectly)
 from the specified class.
 """
-
-
 def inherits_from(obj, a_class):
     """Check if obj is an instance of a class
     that inherited (directly or indirectly)
