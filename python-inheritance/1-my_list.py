58cbd38660a9c431246bbdc74204f36fd4fdac57
Bree-Coding <brendapollagba@gmail.com>
2025-02-07 20:28:17 +0100

    docs: add module docstring for MyList class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Module that defines a subclass of list with a method to print the list in sorted order."""
+
+
 class MyList(list):
     """A subclass of list that provides a method
     to print the list in sorted order.
