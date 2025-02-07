02ca0ff7b880f311f049fe826f248714afa0a23b
Bree-Coding <brendapollagba@gmail.com>
2025-02-07 22:29:36 +0100

    fix: update argument message format in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if number == 0:
         print("0 arguments.")
     elif number == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments".format(number))
 
