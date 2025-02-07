617c4d6f60dc0f7e6366721ba7fc815b5b2b5623
Bree-Coding <brendapollagba@gmail.com>
2025-02-07 22:03:31 +0100

    refactor: simplify docstring and improve clarity of inherits_from function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 
 """Module that defines a function to check if an object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class.
+is an instance of a class
 """
 def inherits_from(obj, a_class):
     """Check if obj is an instance of a class
-    that inherited (directly or indirectly)
-    from the specified class.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to match the type of obj to.
 
     Returns:
-        True if obj is an instance of a class that inherited
-        from the specified class; otherwise False.
+        True if obj is an instance inherited; otherwise False.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
