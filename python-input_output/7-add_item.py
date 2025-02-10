611303fdef8500cf8377e558d247f10eece726ff
Bree-Coding <brendapollagba@gmail.com>
2025-02-10 20:34:43 +0100

    refactor: simplify error handling and improve code readability in add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,19 +7,16 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """
 Try to load the existing list from the JSON file
+If the file does not exist, initialize an empty list
 """
 
 
 try:
     lst = load_from_json_file("add_item.json")
-    """
-    If the file does not exist, initialize an empty list
-    """
+
 except FileNotFoundError:
     lst = []
-"""
-Add each command line argument to the list
-"""
+
 for x in range(1, len(sys.argv)):
     lst.append(sys.argv[x])
 """
