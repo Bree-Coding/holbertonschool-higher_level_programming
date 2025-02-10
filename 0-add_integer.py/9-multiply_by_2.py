595cdd2218886df64d8eb4235c3942d288de80e6
Bree-Coding <brendapollagba@gmail.com>
2025-02-10 09:56:08 +0100

    remove unused Python files and their corresponding README

--- 0-add_integer.py/9-multiply_by_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def multiply_by_2(a_dictionary):
-
-    new_dictionary = a_dictionary.copy()
-    for x, y in a_dictionary.items():
-        new_dictionary[x] = y * 2
-    return new_dictionary
