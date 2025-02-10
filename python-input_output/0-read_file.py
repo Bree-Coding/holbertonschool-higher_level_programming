af7b1881edec27ea46a1e44e8e49189971a49dda
Bree-Coding <brendapollagba@gmail.com>
2025-02-10 13:26:06 +0100

    refactor: improve docstrings for read_file and write_file functions

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module provides a function to read a text file (UTF8) and print it to stdout.
+This module provides a function to read a text file (UTF8)
+and print it to stdout.
 """
 
 
@@ -9,7 +10,8 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+        Defaults to an empty string.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+This module provides a function to write a string to a text file (UTF8)
+and return the number of characters written.
+"""
+
+
 def write_file(filename="", text=""):
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(text)
-        return len(text)
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters
+    written.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
