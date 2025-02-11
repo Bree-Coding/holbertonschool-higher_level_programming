568102e44fc0b51d5df16cb5b97f2b6db08d9b5c
Bree-Coding <brendapollagba@gmail.com>
2025-02-11 10:02:07 +0100

    refactor: improve code documentation and remove unnecessary comments in add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+"""
+Import functions to save and load JSON objects
+"""
 import sys
-from os import path
 
-# Import functions to save and load JSON objects
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# Try to load the existing list from the JSON file
-# If the file does not exist, initialize an empty list
 try:
     lst = load_from_json_file("add_item.json")
 except FileNotFoundError:
     lst = []
 
-# Add each command line argument to the list
 for x in range(1, len(sys.argv)):
     lst.append(sys.argv[x])
 
-# Save the updated list to the JSON file
 save_to_json_file(lst, "add_item.json")
