10b1b2f925859b7e355df66a10edd9ee7fce85ae
Bree-Coding <brendapollagba@gmail.com>
2025-02-14 15:55:12 +0100

    feat: add XML serialization and deserialization functions for Python dictionaries

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """
+Module for serializing and deserializing Python dictionaries to and from XML.
 """
 
 
@@ -9,10 +10,29 @@ import xml.etree.ElementTree as ET
 
 def serialize_to_xml(dictionary, filename):
     """
+    Serializes a Python dictionary to an XML file.
     """
     root = ET.Element('data')
-    
-    
+
+    for key, value in dictionary.items():
+        item = ET.SubElement(root, 'item', key=key)
+        item.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
 def deserialize_from_xml(filename):
     """
+    Deserializes an XML file to a Python dictionary.
     """
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    dictionary = {}
+    for item in root.findall('item'):
+        key = item.get('key')
+        value = item.text
+        dictionary[key] = value
+
+    return dictionary
